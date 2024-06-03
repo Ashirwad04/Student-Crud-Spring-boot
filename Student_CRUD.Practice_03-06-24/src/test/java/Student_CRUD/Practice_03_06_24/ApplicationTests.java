d16c7d910a6ddefd84c40619c2d3e4d48dc40a04
Ashirwad04 <ashirwadk04@gmail.com>
2024-06-03 23:49:31 +0530

    First Commit

---
+++ Student_CRUD.Practice_03-06-24/src/test/java/Student_CRUD/Practice_03_06_24/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Student_CRUD.Practice_03_06_24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
